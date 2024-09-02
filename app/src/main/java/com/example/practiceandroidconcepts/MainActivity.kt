94c542ebaa61dfb015de1732647ce6128b9cc345
2535ShekharMourya <113845874+2535ShekharMourya@users.noreply.github.com>
2024-09-02 09:52:49 +0530

    ui layouts

---
+++ app/src/main/java/com/example/practiceandroidconcepts/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.practiceandroidconcepts
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.image_v).setOnClickListener {
+            startActivity(Intent(this,Main2Activity::class.java))
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
